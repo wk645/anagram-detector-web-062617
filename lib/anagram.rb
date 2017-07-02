3abcd61f72cdf22b08cb67d3e91c2e854d4b4889
wk645 <wk645@nyu.edu>
2017-07-02 19:33:54 -0400

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+
+# %w(foo bar) is a shortcut for ["foo", "bar"]
+class Anagram
+
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(array_of_words)
+		array_of_words.select do |word|
+			@word.split("").sort == word.split("").sort
+			# see if the words taken as the argument are equal to the words of the Anagram class
+		end
+	end
+
+
+
+end
